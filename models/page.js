9fdcf8032ecd575580176c34df60050b58ab4c22
Piyush Kathuria <piyush_kathuria@comcast.com>
2016-11-14 18:52:30 -0500

    Done with About Page

--- models/page.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var webPageSchema = new mongoose.Schema({
-    user: String,
-    html: String,
-    nextid: Number,
-});
-
-module.exports = mongoose.model('webpage', webPageSchema);
