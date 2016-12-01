9a32604dcced8373216f8526cce88dfeb6631ec1
nox <abelogla@buffalo.edu>
2016-12-01 18:11:10 -0500

    Pre-deployment config changes

--- helpers/config.js
+++ helpers/config.js
@@ -11,17 +11,15 @@ module.exports = {
     auth: {
 
         fb: {
-            'clientID': 1799987136944506,
+            'clientID': process.env.FACEBOOK_CLIENTID || 1828026720807214,
             'clientSecret': process.env.FACEBOOK_SECRET,
-            'callbackURL': 'https://buildpage.herokuapp.com/auth/facebook/callback'
-            // 'callbackURL': 'http://localhost:3000/auth/facebook/callback'
+            'callbackURL': process.env.FACEBOOK_CALLBACK || 'http://localhost:3000/auth/facebook/callback'
         },
 
         google: {
-            'clientID': '488115527391-hn8rp7bo07js8p567t5v0i5r9mi7v2g9.apps.googleusercontent.com',
+            'clientID': process.env.GOOGLE_CLIENTID || '488115527391-hn8rp7bo07js8p567t5v0i5r9mi7v2g9.apps.googleusercontent.com',
             'clientSecret': process.env.GOOGLE_SECRET,
-            'callbackURL': 'https://buildpage.herokuapp.com/auth/google/callback'
-            // 'callbackURL': 'http://localhost:3000/auth/google/callback'
+            'callbackURL': process.env.GOOGLE_CALLBACK || 'http://localhost:3000/auth/google/callback'
         }
     }
 };
