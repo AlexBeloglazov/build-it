38c3eb18e091132c414aeae032c7693b7877971e
nox <abelogla@buffalo.edu>
2016-12-01 17:41:32 -0500

    Pre-deployment config changes

--- helpers/config.js
+++ helpers/config.js
@@ -6,20 +6,22 @@ module.exports = {
         saveUninitialized: false
     },
 
-    db: { url: process.env.MONGODB_ULI || 'mongodb://localhost:27017/buildit'},
+    db: { url: process.env.MONGODB_URI || 'mongodb://localhost:27017/buildit'},
 
     auth: {
 
         fb: {
             'clientID': 1799987136944506,
             'clientSecret': process.env.FACEBOOK_SECRET,
-            'callbackURL': 'http://localhost:3000/auth/facebook/callback'
+            'callbackURL': 'https://buildpage.herokuapp.com/auth/facebook/callback'
+            // 'callbackURL': 'http://localhost:3000/auth/facebook/callback'
         },
 
         google: {
             'clientID': '488115527391-hn8rp7bo07js8p567t5v0i5r9mi7v2g9.apps.googleusercontent.com',
             'clientSecret': process.env.GOOGLE_SECRET,
-            'callbackURL': 'http://localhost:3000/auth/google/callback'
+            'callbackURL': 'https://buildpage.herokuapp.com/auth/google/callback'
+            // 'callbackURL': 'http://localhost:3000/auth/google/callback'
         }
     }
 };
